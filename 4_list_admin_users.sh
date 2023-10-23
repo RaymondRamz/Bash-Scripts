089bc44375e4ad9371fd8b05a94f9e734fcb042f
RaymondRamz <75719422+RaymondRamz@users.noreply.github.com>
2023-10-22 22:56:42 -0600

    added comments in script 4

--- 4_list_admin_users.sh
+++ 4_list_admin_users.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
+#Shows Systems Users
 echo "================================"
 echo "System Users:"
 cut -d: -f1 /etc/passwd
 
+#Shows Systems Groups
 echo "================================"
 echo "System Groups:"
 cut -d: -f1 /etc/group
 echo "================================"
 
+#REGEX is used to find users with admin priveleges in sudoers file
 admins=$(sudo grep -P '^(\S+)\s+ALL=\(ALL:ALL\) ALL' /etc/sudoers)
-la
+
+#Stored in admins is used to show the info
 if [ -n "$admins" ]; then
   echo "Users with admin privileges:"
   echo "$admins"
